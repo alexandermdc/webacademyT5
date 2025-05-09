0844983eea3c6f625c20deee6dfebf9c6f5235da
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2025-05-08 20:58:41 -0400

    primeira aula de node

---
+++ NODE/index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const dotenv = require('dotenv');
+
+dotenv.config({path: `./.env`${process.env.NODE_ENV}});
+//console.log(process.env);
+
+const PORT = process.env.PORT ?? 3000;
+const diretorio = process.argv[2] || './';
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {"content-type": "text/html; charset=utf-8"});
+
+    fs.readdir(diretorio, (err, files) => {
+        if (err) {
+            console.error('Diretorio errado', err);
+            res.write('Diretorio errado!!');
+            res.end();
+            return;
+        }
+        files.forEach(file => {
+            res.write(`<p>${file}</p>`);
+        });
+
+        res.end();
+    });
+});
+
+server.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
