adaf88a68455ba4f019a3e02971c7be4849a9822
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2025-05-13 19:54:36 -0400

    iniciando typescript

---
+++ TYPESCRIPT/index.js
@@ -0,0 +1,7 @@
+var username = "John Doe";
+console.log(username);
+var a = 12;
+var b = '12';
+var c = 2;
+console.log(a / c);
+console.log(a / c);
---
+++ TYPESCRIPT/index.ts
@@ -0,0 +1,9 @@
+let username : string = "John Doe";
+console.log(username);
+
+let a: number = 12;
+let b: string = '12'
+let c: number = 2
+
+console.log(a / c);
+console.log(a / c);
