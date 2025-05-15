11bb43cbbf934559e90111d72ea60db0c1db0c7f
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2025-05-14 23:38:32 -0400

    parte 6 do exercecicio ajuste

--- NODE/index.js
+++ NODE/index.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
+const {createLink} = require('./util.js');
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 const PORT = process.env.PORT || 3333;
@@ -10,35 +11,38 @@ const diretorio = process.argv[2] || './';
 const server = http.createServer((req, res) => {
     const url = decodeURIComponent(req.url);
     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
     if (url === '/' || url === '/index') {
         fs.readdir(diretorio, (err, files) => {
             if (err) {
                 res.write('<h1>Erro ao ler o diretório</h1>');
                 return res.end();
             }
-            res.write('<h1>Arquivos disponíveis:</h1>');
+
             files
                 .filter(file => path.extname(file) === '.txt')
                 .forEach(file => {
-                    res.write(`<p><a href="/${file}">${file}</a></p>`);
+                    res.write(createLink(file)); // usando a função
                 });
+
             res.end();
         });
 
     } else {
         const filepath = path.join(diretorio, url);
+
         fs.readFile(filepath, 'utf-8', (err, data) => {
             if (err) {
                 res.write('<h1>Arquivo não encontrado</h1>');
                 return res.end();
             }
-            res.write(`<a href="/">Voltar</a>`);
+
+            res.write(`<a href="/">Voltar</a><br><br>`);
             res.write(`<pre>${data}</pre>`);
             res.end();
         });
     }
 });
-
 server.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
     console.log(`http://localhost:${PORT}`);
---
+++ NODE/util.js
@@ -0,0 +1,5 @@
+
+function createLink(fileName) {
+    return `<a href="/${fileName}">${fileName}</a><br>`;
+}
+module.exports = { createLink };
