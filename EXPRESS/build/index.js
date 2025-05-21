d4048294563c1344e6b0306111bdfd51549e65cc
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2025-05-20 20:06:33 -0400

    continuaçaõ da aula

---
+++ EXPRESS/build/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const validateEnv_1 = __importDefault(require("./utils/validateEnv"));
+dotenv_1.default.config();
+(0, validateEnv_1.default)();
+const PORT = process.env.PORT || 3001;
+const app = (0, express_1.default)();
+app.get('/', (req, res) => {
+    res.send('Hello World! hoje');
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -5,7 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const validateEnv_1 = __importDefault(require("./utils/validateEnv"));
 dotenv_1.default.config();
+(0, validateEnv_1.default)();
 const PORT = process.env.PORT || 3001;
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
