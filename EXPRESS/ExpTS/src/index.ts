c518f3d9bc50ad579fd582615d0f172d3355e3cc
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2025-05-29 11:43:33 -0400

    Exercicio de Handlebars

--- EXPRESS/ExpTS/src/index.ts
+++ EXPRESS/ExpTS/src/index.ts
@@ -2,17 +2,25 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import validateEnv from './utils/validateEnv';
 import morgan from "morgan";
+import router from "./routes/router";
 import logMiddleware from "./utils/logMiddleware";
 import routerLorem from "./routes/lorem";
+import { engine } from "express-handlebars";
+
 dotenv.config();
 validateEnv();
+
 const app = express();
 const PORT = process.env.PORT || 3333;
 app.use(morgan("short"));
 app.use(logMiddleware("complete"));
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello world!");
-});
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", `${__dirname}/views`);
+
+
+app.use("/",router);
 app.use("/lorem", routerLorem);
 app.listen(PORT, () => {
     console.log(`Express app iniciada na porta ${PORT}.`);
--- EXPRESS/ExpTS/src/routes/router.ts
+++ EXPRESS/ExpTS/src/routes/router.ts
@@ -1,1 +1,38 @@
-import Router from 'express';
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+    res.send("Hello world! 1 2 3");
+});
+
+router.get("/hb1", (req: Request, res: Response) => {
+    res.render("hb1", {
+        mensagem: 'olá você está aprendendo Express com HBS',
+        layout: false,
+    });
+});
+
+router.get("/hb2", (req: Request, res: Response) => {
+    res.render("hb2", {
+        poweredByNodejs: true,
+        name: 'Express',
+        type: 'Framework',
+        layout: false,
+
+    });
+});
+
+router.get("/hb3", (req: Request, res: Response) => {
+    const professores = [
+        { nome: "David Fernandes", sala: 1238 },
+        { nome: "Horácio Fernandes", sala: 1239 },
+        { nome: "Edleno Moura", sala: 1240 },
+        { nome: "Elaine Harada", sala: 1241 },
+    ];
+    res.render("hb3", {
+        professores,
+        layout: false,
+    });
+});
+export default router;
