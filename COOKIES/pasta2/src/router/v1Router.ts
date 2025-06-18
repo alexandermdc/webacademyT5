fd55bfa02f97df77be884420edb9704aee3aee33
Alexander Nascimento <anmc@icomp.ufam.edu.br>
2025-06-17 21:15:00 -0400

    aula 17/06, falta ajuste  s

---
+++ COOKIES/pasta2/src/router/v1Router.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import productRouter from "../resources/product/product.router";
+import producrArrayRouter from "../resources/productArray/product.router";
+
+const router = Router();
+
+router.use("/products", productRouter);
+router.use("/productscreate", producrArrayRouter);
+
+export default router;
